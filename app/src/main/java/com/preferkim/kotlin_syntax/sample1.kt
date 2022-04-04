6a2a1ed309a3f470558231c5876e0881a188c66a
Balparang <shineecard5@gmail.com>
2022-04-04 23:18:48 +0900

    [추가] 4. 조건식 if, 삼항연산자, when의 용법

--- app/src/main/java/com/preferkim/kotlin_syntax/sample1.kt
+++ app/src/main/java/com/preferkim/kotlin_syntax/sample1.kt
@@ -1,8 +1,7 @@
 package com.preferkim.kotlin_syntax
 
 fun main() {
-
-    stringTemplate()
+    checkNum(1)
 }
 
 // 1. 함수
@@ -31,4 +30,42 @@ fun stringTemplate(): Unit {
     println("my name is ${name + lastName} I'm 23")
 
     println("\$a") // '$'를 쓰고 싶으면 앞에 백슬래쉬 하나
+}
+
+// 4. 조건식 - if
+fun maxBy(a: Int, b: Int): Int {
+    if (a > b) {
+        return a
+    } else {
+        return b
+    }
+}
+
+// Java의 삼항 연산자 : return (a > b) ? a : b // 참일 때 a, 거짓일 때 b 리턴
+// 코틀린에서는 삼항 연산자가 없기 때문에 아래처럼 쓴다
+fun maxBy2(a: Int, b: Int) = if (a > b) a else b
+
+// 조건식 - when
+fun checkNum(score: Int) { // 리턴타입 없음
+    when (score) {
+        0 -> println("this is 0")
+        1 -> println("this is 1")
+        2, 3 -> println("this is 2 or 3")// 복수 인정 가능
+        else -> println("this is else")
+    }
+
+    // when은 return식으로도 사용이 가능하다. 다만 이때는 else를 무조건 써줘야한다.
+    var b = when (score) {
+        1 -> 1
+        2 -> 2
+        else -> 3
+    }
+    println("b : ${b}")
+
+    when (score) {
+        in 90..100 -> println("You are genius") // when 90 <= score <= 100, println("You are genius")
+        in 10..80 -> println("not bad")
+        else -> println("okay")
+    }
+
 }
